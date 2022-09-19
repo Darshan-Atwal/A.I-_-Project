c2155ca0f10faf10292943bb0213c08944d3c373
DottedLord <113490528+DottedLord@users.noreply.github.com>
2022-09-19 08:26:54 +0530

    Added web search
    Added web search using pywhatkit

--- health.py
+++ health.py
@@ -1,13 +1,16 @@
 import time
 import webbrowser as wb
+import pywhatkit as pwt
 import speech_recognition as sr
 import os
+import wikipedia as googlescrap
 import pyautogui as pg
 import datetime
 import pyttsx3
 engine=pyttsx3.init()
 #code
 def say(name):
+    print(name)
     engine.say(name)
     engine.runAndWait()
 __timenow__=datetime.datetime.now().hour
@@ -17,25 +20,24 @@ if __clientgender__=='male' or __clientgender__=='Male' or __clientgender__=='fe
     pass
 else:
     time.sleep(0.9)
-    print('Unknown Gender, Gender authentification failed.')
-    say('Unknow. gender, Gender authentification failed')
+    say('Unknow. gender. Gender authentification failed')
 os.system('clear')
 time.sleep(0.9)
 print("initialising....")
 time.sleep(0.9)
 if __timenow__>=12 and __timenow__<=16:
-    print("Good Afternoon "+__clientname__+"!")
     say("Good Afternoon "+__clientname__+"!")
 elif __timenow__>16 and __timenow__<24:
-    print('Good evening '+__clientname__+"!")
     say('Good evening '+__clientname__+'!')
 elif __timenow__>=0 and __timenow__<12:
-    print('Good morning '+__clientname__+'!')
     say('Good morning '+__clientname__+'!')
-print('I am your health care assistant!')
+time.sleep(0.5)
+
 say('i am your health care assistant')
-print('My name is Jack')
+time.sleep(0.5)
+
 say('my name is jack!')
+#trial change
 def takecommand():
     try:
         r=sr.Recognizer()
@@ -45,5 +47,19 @@ def takecommand():
         print(e)
     except AttributeError as ae:
         print(ae)
-takecommand()
-#trial commit from tablet
+print('How can i help you?\n: ')
+while True:
+    inp=input('--> ')
+    if inp=='google search':
+        print('what do you want to search: ')
+        inps=input('--> ')
+        time.sleep(0.9)
+        say("This is what i found on the web!")
+        try:
+            pwt.search(inps)
+            result=googlescrap.summary(inps,3)
+            say(result)
+        except Exception as e:
+            say('Cant Call the Data')   
+
+        
