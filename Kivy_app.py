fcd51922a74dfac3f629f8e60ab526350eedfe1e
DottedLord <113490528+DottedLord@users.noreply.github.com>
2022-10-28 18:04:40 +0530

    Update Kivy_app.py

--- Kivy_app.py
+++ Kivy_app.py
@@ -1,6 +1,9 @@
 from kivymd.app import MDApp
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
-
+try:
+    import pywhatkit as pwt
+except Exception:
+    pass
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
@@ -10,12 +13,15 @@ from kivy.core import window
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.toolbar import MDTopAppBar
+import webbrowser as wb
 window.android
 
 screen_helper='''
 ScreenManager:
     Client_Info:
     Client_Service:
+    hint:
+
     
 
 <Client_Info>:
@@ -37,7 +43,7 @@ ScreenManager:
         width: 200
     
     MDTextField:
-        id: city
+        id: location
         helper_text: "Your City"
         pos_hint: {'center_x':0.5, 'center_y':0.6}
         size_hint: (0.5, 0.1)
@@ -59,9 +65,35 @@ ScreenManager:
     MDRectangleFlatButton:    
         text: 'Submit'
         pos_hint: {'center_x':0.5, 'center_y':0.2}
-        on_press: root.manager.current = 'Client_Service' 
-        
+        on_press:  root.manager.current = 'Client_Service'
          
+        
+<hint>:
+    name: 'hint'
+    
+    MDRectangleFlatButton:
+        text: 'Back'
+        on_press: root.manager.current = 'Client_Service'
+        pos_hint: {'center_y':0.19, 'center_x':0.9} 
+    MDTopAppBar:
+        title: "Helper Text"
+    Widget:
+
+    MDLabel:
+        text: ":- Type 'Search' to search on Google"
+        pos_hint: {'center_y':0.7}  
+    MDLabel:
+        text: ':- Type "cure a disease" to find an cure'
+        pos_hint: {'center_y':0.6}    
+    MDLabel:
+        text: ':- Type "Indian healthcare website" for accessing the indian healthcare website'
+        pos_hint: {'center_y':0.5}      
+    MDLabel:
+        text: ':- Type "Hospitals near me" to see the nearest hospitals'
+        pos_hint: {'center_y':0.4} 
+    MDLabel:
+        text: ':- Type "Diagonse a disease" to Diagnose a disease'
+        pos_hint: {'center_y':0.3} 
         
             
 <Client_Service>:
@@ -74,12 +106,31 @@ ScreenManager:
         
                     elevation: 5
                     title: 'HealthyLiving'
-                    left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
+                    
                 Widget:
 
             MDRectangleFlatButton:
                 text: 'Back'
                 on_press: root.manager.current = 'Client_Info' 
+            
+            MDRectangleFlatButton:
+                text: 'Hint'
+                on_press: root.manager.current = 'hint'
+                pos_hint: {'center_x': 0.9}
+            MDTextField:
+                id: inp
+                pos_hint: {'center_y':0.5, 'center_x': 0.5} 
+                size_hint: (0.5,0.1)
+                hint_text: 'User Input'
+           
+                
+
+            MDRectangleFlatButton:
+                text: 'Submit'
+                pos_hint: {'center_y':0.4, 'center_x': 0.5}
+                on_press: root.onclck() 
+                
+
     MDNavigationDrawer:
         id: nav_drawer
               
@@ -90,20 +141,51 @@ ScreenManager:
 class Client_Info(Screen):
     pass
 
+
 class Client_Service(Screen):
+    
+    def onclck(self):
+        
+        inp = self.ids.inp.text
+        if inp=='search' or inp=='Search':
+            self.ids.inpu.text=""
+            wb.open_new_tab("www.google.com")
+          
+        elif inp=="Indian healthcare website":
+            wb.open_new_tab('https://www.mohfw.gov.in/')
+            
+        
+        elif inp=='cure a disease' or inp=='Cure A disease' or inp=='Cure a Disease' or inp=='Cure A Disease':
+            wb.open_new_tab('https://medlineplus.gov')
+            
+        elif inp=='Diagnose a disease' or inp=='diagnose a disease' or inp=='Diagnose A Disease' or inp=="Diagnose a Disease":
+            wb.open_new_tab('https://symptomate.com/diagnosis/0')
+            
+        elif inp=="Hospitals near me" or inp=="hospitals near me" or inp=="Hospitals Near Me" or inp=='Hospitals Near me':
+            pwt.search("Hospitals near me")
+            
+        
+class hint(Screen):
     pass
 
 sm = ScreenManager()
 sm.add_widget(Client_Info(name="Client_Info"))
 sm.add_widget(Client_Service(name='Client_Service'))
 
+
+
 class HealthyLiving(MDApp):
     def build(self):
-        
-        self.screen=Builder.load_string(screen_helper)
-        return self.screen
-    def navigation_draw(self):
-        print('lmao')
 
+        self.screen=Builder.load_string(screen_helper)
+        
+        return self.screen
+    
+
+ 
+    def onclick(self, obj):
+        location = self.ids.city.text        
+        name = self.ids.Name.text
+        gender = self.ids.gender.text
 
 HealthyLiving().run()
